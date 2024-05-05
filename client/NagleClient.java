7404dc6217a78cb7147731252243e1087768294a
jacky <suoema@qq.com>
2024-05-05 18:50:42 +0800

    更新 Nagle 客户端代码。

--- client/NagleClient.java
+++ client/NagleClient.java
@@ -8,14 +8,14 @@ import java.util.concurrent.TimeUnit;
 
 public class NagleClient {
 
-    private static final String HOST = "";
+    private static final String HOST = "106.15.72.83";
     
     private static final int PORT = 9999;
 
     public static void main(String[] args) throws Exception {
         Socket socket = new Socket();
         socket.setTcpNoDelay(true);
-        socket.connect(new InetSocketAddress("172.31.8.118", 9999));
+        socket.connect(new InetSocketAddress(HOST, PORT));
         OutputStream out = socket.getOutputStream();
         byte[] request = new byte[10];
         for (int i = 0; i < 5; i ++) {
